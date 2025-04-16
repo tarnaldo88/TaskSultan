62f77254b6058ed42be1325fafa112a84d7e606e
tarnaldo88 <tarnaldo88@gmail.com>
2025-04-15 23:41:37 -0700

    setting up files for project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to TaskSultan</h1>
+      <p>Your modern SaaS task management app.</p>
+    </div>
+  );
+}
+
+export default App;
